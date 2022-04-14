eceee06477bb689db71df8470c183f7b24f0a81d
James Pacheco <jamesrp13@gmail.com>
2022-04-14 13:57:24 -0600

    Remove functionality for starter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
-import WalletContextProvider from '../components/WalletContextProvider'
 import { AppBar } from '../components/AppBar'
-import { BalanceDisplay } from '../components/BalanceDisplay'
-import { PingButton } from '../components/PingButton'
 import Head from 'next/head'
 
 const Home: NextPage = (props) => {
@@ -17,12 +14,10 @@ const Home: NextPage = (props) => {
           content="Wallet-Adapter Example"
         />
       </Head>
-      <WalletContextProvider>
         <AppBar />
         <div className={styles.AppBody}>
-          <PingButton />
+          { /* Ping button should go here */ }
         </div>
-      </WalletContextProvider >
     </div>
   );
 }
