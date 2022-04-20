8767b73d9801028f0bf95b1d70191fca880a59d0
James Pacheco <jamesrp13@gmail.com>
2022-04-20 10:44:31 -0600

    Update WalletContextProvider.tsx

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -1,25 +1,23 @@
-
-import { FC, ReactNode } from "react";
+import { FC, ReactNode } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import * as web3 from '@solana/web3.js'
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import * as walletAdapterWallets from '@solana/wallet-adapter-wallets';
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const wallet = new PhantomWalletAdapter()
+  const endpoint = web3.clusterApiUrl('devnet')
+	const wallets = [new walletAdapterWallets.PhantomWalletAdapter()]
 
-    const endpoint = web3.clusterApiUrl('devnet')
-
-    return (
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={[wallet]}>
-                <WalletModalProvider>
-                    { children }
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    )
+	return (
+		<ConnectionProvider endpoint={endpoint}>
+	    <WalletProvider wallets={wallets}>
+	      <WalletModalProvider>
+	        { children }
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+	)
 }
 
 export default WalletContextProvider
